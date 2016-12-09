a5e8a4a1ceb606a4e49de09d8fd8c8636b66916f
Egor Frolov <adromaryn@gmail.com>
2016-12-09 23:11:42 +0300

    fix crawler

--- homeworks/crawler/go.pl
+++ homeworks/crawler/go.pl
@@ -10,8 +10,9 @@ use URI;
 use List::Util  qw(sum min);
 
 my $NUM = 10000;
-my $uri = "https://habrahabr.ru/";
+#my $uri = "https://habrahabr.ru/";
 #my $uri = "http://reddit.com";
+my $uri = "http://perldoc.perl.org/";
 my $host = URI->new($uri)->host;
 my %hash = ();
 my $counter = 1;
@@ -19,18 +20,37 @@ my $counter = 1;
 $AnyEvent::HTTP::MAX_PER_HOST = 100;
 my $cv = AnyEvent->condvar;
 
+sub say_stats {my $sum = sum(values %hash);
+  my $size = grep {$hash{$_} != 0} keys %hash;
+  say "Сумма размеров $size страниц: $sum bytes";
+  my @keys = (sort { -($hash{$a} <=> $hash{$b}) } grep {$hash{$_} != 0} keys %hash)[0..min(9,$size-1)];
+  say "Рейтинг:";
+  foreach my $key (@keys) {
+	  my $s = $hash{$key};
+	  say "$key -> $s";
+  }
+}
+
 sub async {
 	my $link = shift;
 	my $cb = shift;
 	my $cr = shift;
 	$counter++;
-	say "$counter страниц в обработке, " . keys(%hash) . " всего запрошено; start";
+	say "$counter страниц в обработке, " . keys(%hash) . " всего запрошено; " .  (grep {$hash{$_} != 0} keys(%hash)) . " получено start";
 	my $w; $w = http_get $link, sub {
 		undef $w;
-		my ($html) = @_;
-		$cb->($link, $html, $cr+1);
-		say "$counter страниц в обработке, " . keys(%hash) . " всего запрошено; exit";
+		my ($body, $hdr) = @_;
+		if ($hdr->{Status} =~ /^2/ ) {
+			$cb->($link, $body, $cr+1)
+		} else {
+			delete $hash{$link};
+		};
+		say "$counter страниц в обработке, " . keys(%hash) . " всего запрошено; " .  (grep {$hash{$_} != 0} keys(%hash)) . " получено exit";
 		$counter--;
+		if ($counter eq 0 or (grep {$hash{$_} != 0} keys(%hash)) == $NUM) {
+			$cv->end;
+			say_stats;
+		}
 	};
 }
 
@@ -38,17 +58,18 @@ my $next; $next = sub {
 	my $link = shift;
 	my $html = shift;
 	my $c = shift;
-	if (defined $html){
+	if (defined $html  and (my $size = grep {$hash{$_} != 0} keys %hash) < $NUM){
 		$hash{$link} = length $html;
 	  my $tree = HTML::TreeBuilder::XPath->new_from_content($html);
-	  my @links = map {(split '\?' , ((split '#', $_)[0]))[0]}
-		            grep {URI->new($_)->host eq $host or URI->new($_)->host eq "www.$host"}
-			          grep {$_ =~ /^http/}
+	  my @links = map {$_ =~ /http/ ? $_ : ($_ =~ /^\// ? "http://$host"."$_" : "http://$host/$_") }
+		            grep {($_ =~ /^http/) &&
+								      (URI->new($_)->host eq $host or URI->new($_)->host eq "www.$host")
+										  or $_ !~ /^#|(?:http)/}
 					  	  grep {defined $_}
 	              map {$_->attr('href')} $tree->findnodes('//a');
 
 	  for my $l (@links) {
-		  if (not defined $hash{$l} and (my $size = keys %hash) < $NUM) {
+		  if (not defined $hash{$l} and (my $size = grep {$hash{$_} != 0} keys %hash) < $NUM) {
 				$hash{$l} = 0;
 				async $l, sub {
 					my $link = shift;
@@ -67,21 +88,9 @@ my $guard; $guard = http_get $uri, sub {
 	my ($res) = @_;
 	$next->($uri, $res, 0);
 	$counter--;
-};
-
-my $t; $t = AE::timer 1, 0.1, sub {
-	if ($counter == 0 ) {
-	  my $sum = sum(values %hash);
-	  my $size = keys %hash;
-	  say "Сумма размеров $size страниц: $sum bytes";
-		my @keys = (sort { -($hash{$a} <=> $hash{$b}) } keys %hash)[0..min(9,$size-1)];
-		say "Рейтинг:";
-		foreach my $key (@keys) {
-			my $s = $hash{$key};
-			say "$key -> $s";
-		}
+	if ($counter eq 0) {
 		$cv->end;
-		undef $t;
+		say_stats;
 	}
 };
 
