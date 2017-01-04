69cbb55a6d5e12654beb1c2c104ae2802ceb1dd2
adromaryn <adromaryn@gmail.com>
2017-01-04 09:55:53 +0300

    Crawler: add version with coroutines (Coro::LWP)

--- homeworks/crawler/cgo.pl
+++ homeworks/crawler/cgo.pl
@@ -3,11 +3,12 @@ use warnings;
 use feature 'say';
 use utf8;
 binmode(STDOUT,':utf8');
-use AnyEvent;
-use AnyEvent::HTTP;
 use HTML::TreeBuilder::XPath;
 use URI;
-use List::Util  qw(sum min);
+use Coro;
+use Coro::LWP;
+use LWP::UserAgent;
+use List::Util  qw(sum min);	
 
 my $NUM = 10000;
 #my $uri = "https://habrahabr.ru/";
@@ -16,10 +17,7 @@ my $uri = "http://perldoc.perl.org/";
 #my $uri = "http://httpbin.org/redirect/6";
 my $host = URI->new($uri)->host;
 my %hash = ();
-my $counter = 1;
-
-$AnyEvent::HTTP::MAX_PER_HOST = 100;
-my $cv = AnyEvent->condvar;
+my $counter = 0;
 
 sub say_stats {my $sum = sum(values %hash);
   my $size = grep {$hash{$_} != 0} keys %hash;
@@ -32,39 +30,36 @@ sub say_stats {my $sum = sum(values %hash);
   }
 }
 
-sub async {
+my $next;
+
+sub upload {
   my $link = shift;
-  my $cb = shift;
-  my $cr = shift;
   $counter++;
   say "$counter страниц в обработке, " . keys(%hash) . " всего запрошено; " .  (grep {$hash{$_} != 0} keys(%hash)) . " получено start" if $counter >= 0;
-  my $w; $w = http_get $link, sub {
-    undef $w;
-    my ($body, $hdr) = @_;
-    if ($hdr->{Status} !~ /^2|3/) {
-      #delete $hash{$link};
-    } elsif ($hdr->{Redirect}) {
-      #delete $hash{$link};
-      if(URI->new($hdr->{URL})->host eq $host) {
-        $cb->($hdr->{URL}, $body, $cr+1);
+  my $ua = LWP::UserAgent->new(agent => 'Coro');
+  my $res = $ua->get($link);
+  if ($res->is_success) {
+    my $fin = $res->request()->uri();
+    if ($fin ne $link) {
+      if (not defined $hash{$fin}) {
+      	$hash{$fin} = 0;
+      	$next->($fin, $res->content);
       }
     } else {
-      $cb->($link, $body, $cr+1);
+      $next->($fin, $res->content);
     }
-    say "$counter страниц в обработке, " . keys(%hash) . " всего запрошено; " .  (grep {$hash{$_} != 0} keys(%hash)) . " получено exit"  if $counter >= 0;
-    $counter--;
-    if ($counter == 0 or ((grep {$hash{$_} != 0} keys(%hash)) == $NUM and $counter > 0)) {
-      $cv->end;
-      $counter = -1;
-      say_stats;
-    }
-  };
+  }
+  say "$counter страниц в обработке, " . keys(%hash) . " всего запрошено; " .  (grep {$hash{$_} != 0} keys(%hash)) . " получено exit"  if $counter >= 0;
+  $counter--;
+  if ($counter == 0 or ((grep {$hash{$_} != 0} keys(%hash)) == $NUM and $counter > 0)) {
+    $counter = -1;
+    say_stats;
+  }
 }
 
-my $next; $next = sub {
+$next = sub {
   my $link = shift;
   my $html = shift;
-  my $c = shift;
   if (defined $html  and (my $size = grep {$hash{$_} != 0} keys %hash) < $NUM){
     $hash{$link} = length $html;
     my $tree = HTML::TreeBuilder::XPath->new_from_content($html);
@@ -76,31 +71,11 @@ my $next; $next = sub {
     for my $l (@links) {
       if (not defined $hash{$l} and (my $size = grep {$hash{$_} != 0} keys %hash) < $NUM) {
       	$hash{$l} = 0;
-      	async $l, sub {
-      	  my $link = shift;
-      	  my $html = shift;
-      	  my $c = shift;
-      	  $next->($link, $html, $c);
-      	}, $c;
+      	upload $l;
       }
     }
   }
 };
 
-$cv->begin;
-my $guard; $guard = http_get $uri, sub {
-  undef $guard;
-  my ($body, $hdr) = @_;
-  if ($hdr->{Redirect}) {
-    $next->($hdr->{URL}, $body, 0);
-  } elsif ($hdr->{Status} == 200) {
-    $next->($uri, $body, 0);
-  }
-  $counter--;
-  if ($counter eq 0) {
-    $cv->end;
-    say_stats;
-  }
-};
+upload $uri;
 
-$cv->recv;
